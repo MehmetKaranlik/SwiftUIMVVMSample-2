040b11456ae2b7e238362b310d7bc8afecaa488a
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2021-12-25 02:36:43 +0300

    First Commit

---
+++ MVVMSample-2/MVVMSample-2/MVVMSample_2App.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMSample_2App.swift
+//  MVVMSample-2
+//
+//  Created by mehmet karanlık on 25.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MVVMSample_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
